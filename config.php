c17d3995c83dbdd066e7bb70e603487644260adf
User Kmusiclife02 <kmusiclife02@www5211.sakura.ne.jp>
2023-05-26 15:40:58 +0900

    Initial COmmit

---
+++ config.php
@@ -0,0 +1,19 @@
+<?php
+
+require 'vendor/autoload.php';
+
+(Dotenv\Dotenv::createImmutable(__DIR__))->load();
+
+$auth0 = new \Auth0\SDK\Auth0([
+    'domain' => $_ENV['AUTH0_DOMAIN'],
+    'clientId' => $_ENV['AUTH0_CLIENT_ID'],
+    'clientSecret' => $_ENV['AUTH0_CLIENT_SECRET'],
+    'cookieSecret' => $_ENV['AUTH0_COOKIE_SECRET']
+]);
+define('ROUTE_URL_INDEX', home_url('/'));
+define('ROUTE_URL_LOGIN', home_url('/login'));
+define('ROUTE_URL_CALLBACK', home_url('/callback'));
+define('ROUTE_URL_LOGOUT', home_url('/logout'));
+
+$session = $auth0->getCredentials();
+
---
+++ init.php
@@ -0,0 +1,28 @@
+<?php
+
+add_action( 'init', function(){
+		
+	$raw_current_url = (is_ssl() ? 'https':'http') . '://' . $_SERVER["HTTP_HOST"] . $_SERVER["REQUEST_URI"];
+	$parsed_url = parse_url($raw_current_url);
+	$encoded_current_url = $parsed_url['scheme'].'://'.$parsed_url['host'].$parsed_url['path'];
+	
+	if( $encoded_current_url == ROUTE_URL_LOGIN ){
+		include('config.php');
+	    $auth0->clear();
+	    header("Location: " . $auth0->login(ROUTE_URL_CALLBACK));
+	    exit;
+    }
+    if( $encoded_current_url == ROUTE_URL_CALLBACK ){
+	    include('config.php');
+	    $auth0->exchange(ROUTE_URL_CALLBACK);
+	    header("Location: " . ROUTE_URL_INDEX);
+	    exit;
+    }
+    if( $encoded_current_url == ROUTE_URL_LOGOUT ){
+		include('config.php');
+		header("Location: " . $auth0->logout(ROUTE_URL_INDEX));
+		exit;
+	}
+
+ 
+});
